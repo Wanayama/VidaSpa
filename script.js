17b1ef6293170603a1d58e6fe10c88ce3c936029
Brian Wanyama <89256205+Wanayama@users.noreply.github.com>
2025-01-18 06:04:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//Menu Toggle
+const navMenu = document.querySelector('.nav_menu');
+const closeMenu = document.querySelector('.closemenu');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+function show(){
+    navMenu.style.display = 'flex';
+    navMenu.style.top = "0";
+}
+function close(){
+    navMenu.style.top = '-100%';
+}
+
+
+
 // /Scroll reveal
 ScrollReveal({
     reset: true,
@@ -5,7 +22,7 @@ ScrollReveal({
     duration: 2000,
     delay: 200
 });
-ScrollReveal().reveal('.hero_content img, .content, .cards', {origin: 'top'});
+ScrollReveal().reveal('.hero_content img, .content', {origin: 'top'});
 ScrollReveal().reveal('.left', {origin: 'left'});
 ScrollReveal().reveal('.right', {origin: 'right'});
-ScrollReveal().reveal('.head, .details', {origin: 'bottom'});
+ScrollReveal().reveal('.head, .details, .cards', {origin: 'bottom'});
